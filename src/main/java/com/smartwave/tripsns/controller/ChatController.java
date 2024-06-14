603b2e82f02e3e747cbe2dd4dac1d20b7ec575b4
chana1226 <eodhldyd7987@gmail.com>
2024-06-14 11:52:48 +0900

    채팅...기능 추가중...

--- src/main/java/com/smartwave/tripsns/controller/ChatController.java
+++ src/main/java/com/smartwave/tripsns/controller/ChatController.java
@@ -2,10 +2,8 @@ package com.smartwave.tripsns.controller;
 
 import com.smartwave.tripsns.service.IF_ChatService;
 import com.smartwave.tripsns.vo.ChatRoomVO;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
--- src/main/java/com/smartwave/tripsns/dao/IF_ChatDAO.java
+++ src/main/java/com/smartwave/tripsns/dao/IF_ChatDAO.java
@@ -2,10 +2,9 @@ package com.smartwave.tripsns.dao;
 
 import com.smartwave.tripsns.vo.ChatRoomVO;
 
-import java.util.List;
 
 public interface IF_ChatDAO {
 
-    public void saveMessage(ChatRoomVO chatRoomVO); // 임시로 만든거임
+//    public void saveMessage(ChatRoomVO chatRoomVO); // 임시로 만든거임
 
 }
